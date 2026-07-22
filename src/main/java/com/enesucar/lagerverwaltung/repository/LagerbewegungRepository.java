08968c4df73ced47afdd5b467656ee75b1869636
Enes <eucardeveloper@gmail.com>
2026-07-22 15:32:02 +0300

    feat: add EntityGraph to fix N+1 problem in LagerbewegungRepository

--- src/main/java/com/enesucar/lagerverwaltung/repository/LagerbewegungRepository.java
+++ src/main/java/com/enesucar/lagerverwaltung/repository/LagerbewegungRepository.java
@@ -1,7 +1,13 @@
 package com.enesucar.lagerverwaltung.repository;
 
 import com.enesucar.lagerverwaltung.entity.Lagerbewegung;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LagerbewegungRepository extends JpaRepository<Lagerbewegung, Long> {
+
+    @EntityGraph(attributePaths = {"produkt"})
+    List<Lagerbewegung> findAll();
 }
